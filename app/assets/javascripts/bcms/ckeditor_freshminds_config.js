a600969f5f3e6b751a6f4379744a16a29f457809
Jan Jagoda <janjagoda@gmail.com>
2015-03-06 00:44:37 +0100

    + General content CKEditor styles

--- app/assets/javascripts/bcms/ckeditor_freshminds_config.js
+++ app/assets/javascripts/bcms/ckeditor_freshminds_config.js
@@ -3,10 +3,15 @@ CKEDITOR.stylesSet.add( 'freshminds_styles', [
     { name: 'Title', element: 'h1', attributes: {'class': 'gc-title'}},
     { name: 'Sub Title', element: 'h2', attributes: {'class': 'gc-sub-title'}},
     { name: 'Section Header', element: 'h2', attributes: {'class': 'gc-section-header'}},
+    { name: 'Sub Section Header', element: 'h3', attributes: {'class': 'gc-sub-section-header'}},
     { name: 'Section Content', element: 'p', attributes: {'class': 'gc-section-content'}},
     { name: 'More Link', element: 'a', attributes: {'class': 'gc-link-more'}},
-    { name: 'Bullet List', element: 'li', attributes: {'class': 'gc-list-bulletpoint'}},
-    { name: 'Quote', element: 'p', attributes: {'class': 'gc-quote'}},
+    { name: 'Image (left aligned)', element: 'img', attributes: {'class': 'gc-img-left'}},
+    { name: 'Image (right aligned)', element: 'img', attributes: {'class': 'gc-img-right'}},
+    { name: 'Bullet List', element: 'ul', attributes: {'class': 'gc-list'}},
+    { name: 'Bullet List (collapsed)', element: 'ul', attributes: {'class': 'gc-list-collapsed'}},
+    { name: 'Quote', element: 'blockquote', attributes: {'class': 'gc-quote'}}
+    
 
 ]);
 
