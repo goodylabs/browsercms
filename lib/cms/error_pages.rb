caa99d3465853bf2c3883646b7fc0f0f052097e6
Paweł Łuczak <woochaq@gmail.com>
2015-04-07 16:39:31 +0200

    Change errora pages urls

--- lib/cms/error_pages.rb
+++ lib/cms/error_pages.rb
@@ -1,8 +1,8 @@
 module Cms
   module ErrorPages
 
-    NOT_FOUND_PATH = '/system/not_found'
-    FORBIDDEN_PATH = '/system/access_denied'
-    SERVER_ERROR_PATH = '/system/server_error'
+    NOT_FOUND_PATH = '/system/not-found'
+    FORBIDDEN_PATH = '/system/access-denied'
+    SERVER_ERROR_PATH = '/system/server-error'
   end
 end
