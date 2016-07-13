abac7debf45f8f0a5ec75452f8781b178cb52f76
Adam Migodziński <adam.migodzinski@goodylabs.com>
2016-07-13 14:25:41 +0200

    #1168 - Ones to Watch in Back End

--- app/models/cms/search_filter.rb
+++ app/models/cms/search_filter.rb
@@ -4,7 +4,7 @@ module Cms
     include ::ActiveModel::Model
     include ::ActiveModel::Serialization
 
-    attr_accessor :model_class, :term, :responsible_person_email, :name, :filled, :job_type_id
+    attr_accessor :model_class, :term, :responsible_person_email, :name, :filled, :job_type_id, :first_name, :surname, :nominator_last_name, :email, :university_id
 
     def self.build(params_hash, model_class)
       model = self.new(params_hash)
@@ -17,7 +17,7 @@ module Cms
     end
 
     def attributes
-      { 'name' => nil, 'responsible_person_email' => nil, 'filled' => nil, 'job_type_id' => nil }
+      { 'name' => nil, 'responsible_person_email' => nil, 'filled' => nil, 'job_type_id' => nil, 'first_name' => nil, 'surname' => nil, 'nominator_last_name' => nil, 'email' => nil, 'university_id' => nil }
     end
 
   end
